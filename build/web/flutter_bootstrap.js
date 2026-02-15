04efaed14bcc1316e258b987fd04ec970514eed5
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-02-15 22:42:19 +0800

    git changing icons again

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3878191950"
+    serviceWorkerVersion: "2093979371"
   }
 });
